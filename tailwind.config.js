2ec29c9c42ddea02937a35cfe616dab20ae0f977
Aman Verma <aman@Amans-MacBook-Air.local>
2023-06-26 14:01:54 +0530

    developed sidebar

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-purple": "#6059e6",
+        "light-white": "#d7dbfa",
+      },
+    },
+  },
+  plugins: [],
+};
